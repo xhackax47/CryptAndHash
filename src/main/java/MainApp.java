0aef579299c6c5c3e77d9428cf1982380f162cab
CHAABI Samy <samy_chaabi@yahoo.fr>
2018-09-29 20:39:39 +0200

    author + Recompil + CryptHash.start()

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,13 @@
 import app.CryptHash;
 
+/**
+ * 
+ * @author CHAABI Samy
+ */
+
 public class MainApp {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {		
 		CryptHash.start();
 	}
 
