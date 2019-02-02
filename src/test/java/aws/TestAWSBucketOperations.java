3ece347364086cefef1cbb230a175739ba713c40
CHAABI Samy <samy_chaabi@yahoo.fr>
2019-02-03 00:13:35 +0100

    Début tests Junit AWS

---
+++ src/test/java/aws/TestAWSBucketOperations.java
@@ -0,0 +1,14 @@
+package aws;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestAWSBucketOperations {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
