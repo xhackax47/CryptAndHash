4533f3cdb14eb610039d11cb947693985cf54031
CHAABI Samy <samy_chaabi@yahoo.fr>
2019-02-06 23:14:30 +0100

    Implémentation AWS EC2

--- src/main/java/aws/AWSApplicationS3.java
+++ src/main/java/aws/AWSApplicationS3.java
@@ -1,3 +1,8 @@
+/**
+ * 
+ * @author CHAABI Samy
+ */
+
 package aws;
 
 import java.io.File;
@@ -21,7 +26,8 @@ import com.amazonaws.services.s3.model.S3ObjectSummary;
 public class AWSApplicationS3 {
 
 	private static String bucketName;
-	private static final AWSCredentials credentials = new BasicAWSCredentials("ACCESS-KEY-AWS","SECRET-KEY-AWS");
+	private static final AWSCredentials credentials = new BasicAWSCredentials("ACCESS-KEY-AWS", "SECRET-KEY-AWS");
+	
 //	AWSCredentials test = new AWSCredentials() {
 //		
 //		@Override
@@ -37,22 +43,20 @@ public class AWSApplicationS3 {
 //		}
 //	};
 
-	public static AmazonS3 connect() {
+	public static AmazonS3 connectS3() {
 		AmazonS3 s3client = AmazonS3ClientBuilder.standard()
 				.withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(Regions.EU_WEST_3).build();
 		return s3client;
 	}
 
-	static AmazonS3 awsService = AWSApplicationS3.connect();
-
 	// Creer un conteneur
 	public static void createBucket(String bucketName) {
 		try {
-			if (awsService.doesBucketExistV2(bucketName)) {
+			if (connectS3().doesBucketExistV2(bucketName)) {
 				System.out.println("Le nom du conteneur est déjà pris." + " Veuillez réessayer avec un autre nom.");
 				return;
 			}
-			awsService.createBucket(bucketName);
+			connectS3().createBucket(bucketName);
 		}
 
 		catch (Exception e) {
@@ -63,25 +67,25 @@ public class AWSApplicationS3 {
 
 	// Lister tous les conteneurs
 	public static void listAllBuckets() {
-		for (Bucket s : awsService.listBuckets()) {
+		for (Bucket s : connectS3().listBuckets()) {
 			System.out.println(s.getName());
 		}
 	}
 
 	// Supprimer un conteneur
 	public static void deleteBucket(String bucketNameDel) {
-		awsService.deleteBucket(bucketNameDel);
+		connectS3().deleteBucket(bucketNameDel);
 	}
 
 	// Uploader un objet sur le conteneur
 	public static void uploadBucket(String bucketNameUp) {
-		awsService.putObject(bucketNameUp, "Tests\\test.pdf",
+		connectS3().putObject(bucketNameUp, "Tests\\test.pdf",
 				new File("C:\\Users\\Megaport\\git\\CryptAndHash\\testsAWS\\test.pdf"));
 	}
 
 	// Lister les objets d'un conteneur
 	public static void listAllObjectsFromBucket(String bucketName) {
-		ObjectListing objectListing = awsService.listObjects(bucketName);
+		ObjectListing objectListing = connectS3().listObjects(bucketName);
 		for (S3ObjectSummary os : objectListing.getObjectSummaries()) {
 			System.out.println(os.getKey());
 		}
@@ -89,7 +93,7 @@ public class AWSApplicationS3 {
 
 	// Telecharger un objet d'un conteneur
 	public static void downloadObjectFromBucket(String bucketName) throws IOException {
-		S3Object s3object = awsService.getObject(bucketName, "Tests\\test.pdf");
+		S3Object s3object = connectS3().getObject(bucketName, "Tests\\test.pdf");
 		S3ObjectInputStream inputStream = s3object.getObjectContent();
 
 		FileUtils.copyInputStreamToFile(inputStream,
@@ -98,12 +102,12 @@ public class AWSApplicationS3 {
 
 	// Copier un objet d'un conteneur
 	public static void copyObjectFromBucket(String bucketName, String destinationBucketName) {
-		awsService.copyObject(bucketName, "Tests\\test.pdf", destinationBucketName, "TestCopy\\test.pdf");
+		connectS3().copyObject(bucketName, "Tests\\test.pdf", destinationBucketName, "TestCopy\\test.pdf");
 	}
 
 	// Supprimer un objet d'un conteneur
 	public static void deleteObjectFromBucket(String bucketName) {
-		awsService.deleteObject(bucketName, "Tests\\test.pdf");
+		connectS3().deleteObject(bucketName, "Tests\\test.pdf");
 	}
 
 	// Supprimer plusieurs objets d'un conteneur
@@ -111,7 +115,7 @@ public class AWSApplicationS3 {
 		String objkeyArr[] = { "Tests\\test.pdf", "TestCopy\\test.pdf" };
 
 		DeleteObjectsRequest delObjReq = new DeleteObjectsRequest(bucketName).withKeys(objkeyArr);
-		awsService.deleteObjects(delObjReq);
+		connectS3().deleteObjects(delObjReq);
 	}
 
 	public static String getBucketName() {
